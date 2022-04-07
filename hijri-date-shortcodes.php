f3cf08173f60f5895db91184a37096ecadee522d
Jeremy Varnham <jeremy.varnham@gmail.com>
2022-04-07 07:15:52 +0300

    Update hijri-date-shortcodes.php
    Version bump

--- hijri-date-shortcodes.php
+++ hijri-date-shortcodes.php
@@ -3,7 +3,7 @@
 Plugin Name: Hijri Date Shortcodes
 Plugin URI: https://github.com/jvarn/hijri-date-shortcodes
 Description: Adds shortcodes for converting between Gregorian and Hijri dates
-Version: 0.1.0
+Version: 0.1.1
 Author: Jeremy Varnham
 Author URI: https://abuyasmeen.com/
 */
