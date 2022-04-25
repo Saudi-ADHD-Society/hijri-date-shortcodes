149ef78d2fa5499474d90a02edcb3dea64a0f785
Jeremy Varnham <jeremy.varnham@gmail.com>
2022-04-25 12:44:04 +0300

    Update hijri-date-shortcodes.php
    Remove class check

--- hijri-date-shortcodes.php
+++ hijri-date-shortcodes.php
@@ -3,7 +3,7 @@
  * Plugin Name: Hijri Date Shortcodes
  * Plugin URI: https://github.com/jvarn/hijri-date-shortcodes
  * Description: Adds shortcodes for converting between Gregorian and Hijri dates
- * Version: 0.2.2
+ * Version: 0.2.3
  * Author: Jeremy Varnham
  * Author URI: https://abuyasmeen.com/
  *
@@ -22,6 +22,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @since 0.1.0
  */
-if ( ! class_exists( 'Converter' ) ) {
-	require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
-}
+
+require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
