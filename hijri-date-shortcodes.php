08dcfd0a86e962dc99ac77dd8042814bba5b2986
Jeremy Varnham <jeremy.varnham@gmail.com>
2022-04-25 09:07:07 +0000

    Moved class into separate file

--- hijri-date-shortcodes.php
+++ hijri-date-shortcodes.php
@@ -1,69 +1,27 @@
 <?php
-/*
-Plugin Name: Hijri Date Shortcodes
-Plugin URI: https://github.com/jvarn/hijri-date-shortcodes
-Description: Adds shortcodes for converting between Gregorian and Hijri dates
-Version: 0.2.1
-Author: Jeremy Varnham
-Author URI: https://abuyasmeen.com/
-*/
-
-namespace jvarn\HijriDateShortcodes;
-
-// Prevent Direct Access
-if ( ! defined( 'ABSPATH' ) ) { 
-	wp_die(); 
-}
-
 /**
- * loads external class (Hijri Date) Converter 
- *
- *	@since 0.1.0
- */
-if ( ! class_exists( 'Converter' ) ) {
-	require_once( plugin_dir_path( __FILE__ ) . 'vendor/autoload.php');
-}
-
-/**
- * Class Shortcodes
+ * Plugin Name: Hijri Date Shortcodes
+ * Plugin URI: https://github.com/jvarn/hijri-date-shortcodes
+ * Description: Adds shortcodes for converting between Gregorian and Hijri dates
+ * Version: 0.2.2
+ * Author: Jeremy Varnham
+ * Author URI: https://abuyasmeen.com/
  *
  * @package jvarn\hijri-date-shortcodes
  */
-class Shortcodes {
 
-	public function __construct() {
-		add_shortcode( 'hijri_date', array( $this, 'jlv_hijri_date_shortcode') );
-	}
+namespace Jvarn;
 
-	public function jlv_hijri_date_shortcode( $atts, $content="" ) {
-		extract( \shortcode_atts( 
-			array( 
-				'rel' => 'now', // now, today, tomorrow, yesterday
-				'gdate' => null, // 2022-01-01
-				'hdate' => null, // 1438-01-01
-				), $atts ) );
-	
-		if ( isset( $gdate ) ) {
-			return \GeniusTS\HijriDate\Hijri::convertToHijri($gdate);
-		} else if ( isset( $hdate ) ) {
-			$hstring = explode( '-', $hdate );
-			return \GeniusTS\HijriDate\Hijri::convertToGregorian($hstring[2], $hstring[1], $hstring[0]);
-		} else {
-			switch ( $rel ) {
-			    case 'now':
-			        return \GeniusTS\HijriDate\Date::now();
-			        break;
-			    case 'today':
-			        return \GeniusTS\HijriDate\Date::today();
-			        break;
-			    case 'tomorrow':
-			        return \GeniusTS\HijriDate\Date::tomorrow();
-			        break;
-			    case 'yesterday':
-			    	return \GeniusTS\HijriDate\Date::yesterday();
-			        break;
-			}
-		}
-	}
+// Prevent Direct Access.
+if ( ! defined( 'ABSPATH' ) ) {
+	wp_die();
 }
-$HirjiDateShortcodes = new Shortcodes();
+
+/**
+ * Loads external class (Hijri Date) Converter.
+ *
+ * @since 0.1.0
+ */
+if ( ! class_exists( 'Converter' ) ) {
+	require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+}
---
+++ src/shortcodes/Shortcodes.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Class Shortcodes
+ *
+ * @package jvarn\hijri-date-shortcodes
+ */
+
+namespace Jvarn\hijridate;
+
+use \GeniusTS\HijriDate\Hijri as Convert;
+use \GeniusTS\HijriDate\Date as Relative;
+
+/**
+ * Shortcode class.
+ */
+class Shortcodes {
+
+	/**
+	 * Construct.
+	 */
+	public function __construct() {
+		add_shortcode( 'hijri_date', array( $this, 'jlv_hijri_date_shortcode' ) );
+	}
+
+	/**
+	 * Shortcode init.
+	 *
+	 * Initializes the shortcode.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array  $atts {
+	 *   Required. Array of WP shortcode arguments.
+	 *
+	 *   @type string    $rel      Relative date. Default now. Accepts today, tomorrow, yesterday.
+	 *   @type string    $gdate    Gregorian date string in yyyy-MM-dd format.
+	 *   @type string    $hdate    Hijri date string in yyyy-MM-dd format.
+	 * }
+	 * @param string $content WP shortcode html contents. Default null.
+	 */
+	public function jlv_hijri_date_shortcode( $atts, $content = '' ) {
+		$defaults = array(
+			'rel'   => 'now',
+			'gdate' => null,
+			'hdate' => null,
+		);
+
+		$atts = \shortcode_atts(
+			$defaults,
+			$atts
+		);
+
+		if ( isset( $atts['gdate'] ) ) {
+			return Hijri::convertToHijri( $atts['gdate'] );
+		} elseif ( isset( $atts['hdate'] ) ) {
+			$hstring = explode( '-', $atts['hdate'] );
+			return \GeniusTS\HijriDate\Hijri::convertToGregorian( $hstring[2], $hstring[1], $hstring[0] );
+		} else {
+			switch ( $atts['rel'] ) {
+				case 'now':
+					return \GeniusTS\HijriDate\Date::now();
+				case 'today':
+					return \GeniusTS\HijriDate\Date::today();
+				case 'tomorrow':
+					return \GeniusTS\HijriDate\Date::tomorrow();
+				case 'yesterday':
+					return \GeniusTS\HijriDate\Date::yesterday();
+			}
+		}
+	}
+}
+$hijri_date_shortcode = new Shortcodes();
